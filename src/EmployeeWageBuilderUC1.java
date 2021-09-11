19618b973bb8d811b4f03f002420d58ae9246b8b
Chethan <shetty.chetu7@gmail.com>
2021-09-11 13:41:37 +0530

    First commit

---
+++ src/EmployeeWageBuilderUC1.java
@@ -0,0 +1,13 @@
+
+public class EmployeeWageBuilderUC1 {
+	public static void main(String[] args) {
+		int IS_FULL_TIME=0;
+		double empcheck = Math.floor(Math.random()*10)%2;
+		if(empcheck==IS_FULL_TIME)
+		
+			System.out.println("Employee is Present");
+			else
+				System.out.println("Employee is Absent");
+	}
+
+}
